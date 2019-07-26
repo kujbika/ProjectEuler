1d2e3f9cd24887fcd0913265ae99a3ff43d24595
kujbika <43244473+kujbika@users.noreply.github.com>
2019-07-26 22:12:26 +0200

    Update on Problem10

---
+++ problem10.py
@@ -0,0 +1,17 @@
+import numpy as np
+def isPrime(number):
+    '''
+    The function takes an integer as input,
+    and outputs either itself or zero, depending on whether
+    it is a prime.
+    '''
+    a = int(np.sqrt(number)) + 1
+    isprime = True
+    for i in range(2,a):
+        if number % i == 0:
+            isprime = False
+            break
+    return number * isprime
+if __name__ == "__main__":
+    primes = [isPrime(x) for x in range(2,2000000)]
+    print(np.sum(primes))
